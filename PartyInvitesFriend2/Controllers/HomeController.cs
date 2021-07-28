04495e5aae4377696c22e9388308c05866acab11
Hüseyin Altun <70349056+Huseyin-altun@users.noreply.github.com>
2021-07-28 20:42:27 +0300

    Add files via upload

---
+++ PartyInvitesFriend2/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartyInvitesFriend2.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvitesFriend2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult Form()
+        {
+            return View();
+
+        }
+
+
+        [HttpPost]
+        public ViewResult Form(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+            }
+            else { 
+                return View(); 
+            }
+        }
+        public ViewResult ListResponses() {
+            return View(Repository.Responses.Where(r => r.WillAttend == true)); 
+        }
+
+
+
+    }
+}
---
+++ PartyInvitesFriend2/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace PartyInvitesFriend2.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Lütfen adınızı giriniz")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "lütfen e-mail adresinizi giriniz")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Lütfen telefon numaranızı girin")]
+
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Lütfen katılacağınızı belirtin")]
+        public bool? WillAttend { get; set; }
+    }
+}
